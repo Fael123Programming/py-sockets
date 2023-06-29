fb6e06c881bb087acd84e5d5d6f27153bb715ea2
Fael123Programming <rafaelfonseca1020@gmail.com>
2023-06-29 14:45:03 +0100

    new files

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 import socket
 
 def run_client():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect(('data.pr4e.org', 80))
-        request = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
-        sock.send(request)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        # client.connect(('data.pr4e.org', 80))
+        # request = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+        client.connect(('localhost', 9000))
+        request = 'GET HTTP/1.0\r\n\r\n'.encode()
+        client.send(request)
         while True:
-            data = sock.recv(512)
+            data = client.recv(512)
             if len(data) < 1:
                 break
             print(data.decode(), end='')
